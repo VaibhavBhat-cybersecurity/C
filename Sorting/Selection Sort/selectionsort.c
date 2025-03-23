d590f36335aca2770e8500e7ca3b8833d09bc657
Cyberbot <vab2424168@sicsr.ac.in>
2025-03-23 19:33:24 +0530

    Update selectionsort.c

--- Sorting/Selection Sort/selectionsort.c
+++ Sorting/Selection Sort/selectionsort.c
@@ -1,18 +1,51 @@
 #include<stdio.h>
-int main()
-{
-    int i,a,n,t,j;
-    for(i=0;i<n-1;i++)
-    {
-        for(j=i+1;j<n;j++)
-        {
-            if(a[i>a[j]])
-            {
-                t=a[i];
-                a[i]=a[j];
-                a[j]=t;
-            }
-        }
+
+void accept(int a[10], int n) {
+    int i;
+    for(i = 0; i < n; i++) {
+        printf("Enter a[%d]:", i);
+        scanf("%d", &a[i]);
     }
-return 0;
+}
+
+void selectionsort(int a[10],int n)
+{
+    int indexmin;
+    for(int i=0;i<n;i++)
+    {
+        indexmin=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]<a[indexmin])
+            {
+                indexmin=j;
+                int t=a[i];
+                a[i]=a[indexmin];
+                a[indexmin]=t;
+            }
+
+        }
+
+    }
+}
+
+void display(int a[10], int n) {
+    int i;
+    for(i = 0; i < n; i++) {
+        printf("%d\t", a[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int a[10], n;
+    printf("Enter the number of elements you want in the array: ");
+    scanf("%d", &n);
+    accept(a, n);
+    printf("Entered Elements are:\n");
+    display(a, n);
+    selectionsort(a, n);
+    printf("Sorted Elements are:\n");
+    display(a, n);
+    return 0;
 }
