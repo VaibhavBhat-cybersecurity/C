2a448daab3e697f39edf5008058f6e93bb7b06e0
Cyberbot <vab2424168@sicsr.ac.in>
2025-04-17 09:22:05 +0530

    Update linkedlist.c

--- List/linkedlist.c
+++ List/linkedlist.c
@@ -1,83 +1,83 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define newnode (struct node *)malloc(sizeof(struct node))
 
-#define newNode (struct node *)malloc(sizeof(struct node)) // Changed macro name to avoid conflict
-
-struct node {
+struct node
+{
     int data;
     struct node *next;
 };
 
-// Function to create a linked list
-struct node* createList(struct node* H, int n) {
-    int i;
+struct node *createlist(struct node *H, int n)
+{
     struct node *s;
-    H = newNode; // Initialize the first node
+    int i;
+    H = newnode;
     H->next = NULL;
-
-    printf("Enter the data\n");
+    printf("Enter the data: ");
     scanf("%d", &H->data);
     s = H;
 
-    for (i = 1; i < n; i++) {
-        s->next = newNode; // Allocate memory for the next node
+    for (i = 1; i < n; i++)
+    {
+        s->next = newnode;
         s = s->next;
-        printf("Enter the data\n");
+        printf("Enter the data: ");
         scanf("%d", &s->data);
     }
-
-    s->next = NULL; // End of list
+    s->next = NULL;
     return H;
 }
 
-// Function to display the linked list
-void display(struct node *H) {
+void display(struct node *H)
+{
     struct node *s = H;
-    printf("Linked list:\n");
-    while (s != NULL) {
+    while (s != NULL)
+    {
         printf("%d\t", s->data);
         s = s->next;
     }
     printf("\n");
 }
 
-// Function to calculate the length of the linked list
-int length(struct node *H) {
+int length(struct node *H)
+{
+    int l = 0;
     struct node *s = H;
-    int l = 0; // Initialize length to 0
-    while (s != NULL) {
+    while (s != NULL)
+    {
         l++;
         s = s->next;
     }
     return l;
 }
 
-// Function to insert a new node at a specified position
-struct node* insert(struct node *H, int x, int pos) {
+struct node *insert(struct node *H, int x, int pos)
+{
     struct node *s, *t;
-    int i;
-
-    t = newNode;
+    t = newnode;
     t->data = x;
     t->next = NULL;
-
     s = H;
 
-    // Case 1: Insert at the beginning (position 1)
-    if (pos == 1) {
+    if (pos == 1)
+    {
         t->next = H;
         H = t;
     }
-    else {
-        // Traverse to the position where we want to insert
-        for (i = 1; i < pos - 1 && s != NULL; i++) {
+    else
+    {
+        for (int i = 1; i < pos - 1 && s != NULL; i++)
+        {
             s = s->next;
         }
-
-        if (s == NULL) {
+        if (s == NULL)
+        {
             printf("Invalid position\n");
+            free(t); // Important: Free the allocated memory if insertion fails
         }
-        else {
+        else
+        {
             t->next = s->next;
             s->next = t;
         }
@@ -85,51 +85,125 @@ struct node* insert(struct node *H, int x, int pos) {
     return H;
 }
 
-int main() {
-    int n, ch, l, x, pos;
+struct node *deletenode(struct node *H, int pos)
+{
+    int i;
+    struct node *s, *t;
+    s = H;
+
+    if (H == NULL)
+    {
+        printf("List is empty!\n"); // Corrected printf format
+        return H;
+    }
+
+    if (pos == 1)
+    {
+        H = H->next;
+        free(s);
+        return H;
+    }
+    else
+    {
+        for (i = 1; i < pos - 1 && s != NULL && s->next != NULL; i++) // Added s != NULL check
+        {
+            s = s->next;
+        }
+
+        if (s == NULL || s->next == NULL) // Corrected condition and added s == NULL check
+        {
+            printf("Invalid position\n"); // Corrected printf format
+            return H;
+        }
+        else
+        {
+            t = s->next;
+            s->next = t->next;
+            free(t);
+        }
+    }
+    return H;
+}
+
+struct node *erase(struct node *H)
+{
+    struct node *s = H;
+    while (s != NULL)
+    {
+        struct node *temp = s;
+        s = s->next;
+        free(temp);
+    }
+    return NULL;
+}
+
+int main()
+{
     struct node *H = NULL;
-
-    while (1) {
+    int choice, n, pos, l, x;
+    while (1)
+    {
         printf("Menu\n");
-        printf("1. Create\n");
-        printf("2. Display\n");
-        printf("3. Length\n");
-        printf("4. Insert\n");
-        printf("5. Exit\n");
-        printf("Enter your choice\n");
-        scanf("%d", &ch);
-
-        switch (ch) {
-            case 1:
-                printf("Enter the number of elements you want in the linked list:\n");
-                scanf("%d", &n);
-                H = createList(H, n); // Create the linked list
-                break;
-            case 2:
-                display(H); // Display the linked list
-                break;
-            case 3:
-                l = length(H);
-                printf("The length of the linked list is %d\n", l); // Display the length
-                break;
-            case 4:
-                printf("Enter the position where you want to insert:\n");
-                scanf("%d", &pos);
-                l = length(H);
-                if (pos < 1 || pos > l + 1) {
-                    printf("Invalid position\n");
-                }
-                else {
-                    printf("Enter the data to be inserted:\n");
-                    scanf("%d", &x);
-                    H = insert(H, x, pos); // Insert the new node
-                }
-                break;
-            case 5:
-                exit(0); // Exit the program
-                break;
-            default:
-                printf("Please enter a correct choice\n");
+        printf("1.Create\n");
+        printf("2.Display\n");
+        printf("3.Length\n");
+        printf("4.Insert\n");
+        printf("5.Delete\n"); // Added Delete option
+        printf("6.Erase List\n"); // Added Erase List option
+        printf("7.Exit\n");     // Corrected Exit option number
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the number of elements you want in the linked list:\n");
+            scanf("%d", &n);
+            H = createlist(H, n); // Create the linked list
+            break;
+        case 2:
+            display(H); // Display the linked list
+            break;
+        case 3:
+            l = length(H);
+            printf("The length of the linked list is %d\n", l); // Display the length
+            break;
+        case 4:
+            printf("Enter the position where you want to insert:\n");
+            scanf("%d", &pos);
+            l = length(H);
+            if (pos < 1 || pos > l + 1)
+            {
+                printf("Invalid position\n");
+            }
+            else
+            {
+                printf("Enter the data to be inserted:\n");
+                scanf("%d", &x);
+                H = insert(H, x, pos); // Insert the new node
+            }
+            break;
+        case 5: // Delete node
+            printf("Enter the position of the node to delete:\n");
+            scanf("%d", &pos);
+            l = length(H);
+            if (pos < 1 || pos > l)
+            {
+                printf("Invalid position\n");
+            }
+            else
+            {
+                H = deletenode(H, pos);
+            }
+            break;
+        case 6: // Erase list
+            H = erase(H);
+            printf("List erased successfully!\n");
+            break;
+        case 7:
+            exit(0); // Exit the program
+            break;
+        default:
+            printf("Please enter a correct choice\n");
         }
     }
     return 0;
